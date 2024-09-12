496c6c6b4ba0d032a17f0a890ea130a69f70b00a
Kirill Gudeta <kirillgudeta@gmail.com>
2024-09-13 00:53:25 +0300

    done 1 esle

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
 module.exports = function check(str, bracketsConfig) {
+	let op = 0 // Для отслеживания состояния символа '|'
 	const matching = {}
+
+	// Создаем таблицу соответствий закрывающих и открывающих скобок
 	bracketsConfig.map(el => {
-		matching[`${el[1]}`] = `${el[0]}`
+		matching[el[1]] = el[0]
 	})
-	console.log(matching)
+
 	const strArr = str.split('')
 	const stack = []
+
+	// Используем map() для обработки каждого элемента строки
 	strArr.map(el => {
 		if (Object.values(matching).includes(el)) {
+			if (el === '|' && op === 0) {
+				op += 1
+			} else if (el === '|' && op === 1) {
+				op -= 1
+				const x = stack.pop()
+				return
+			}
 			stack.push(el)
-		}
-		if (Object.keys(matching).includes(el)) {
+		} else if (Object.keys(matching).includes(el)) {
 			const x = stack.pop()
+			if (x === '|') {
+				op -= 1
+			}
 			if (x !== matching[el]) {
 				stack.push(x)
-				return false
 			}
 		}
 		return el
 	})
+
 	return stack.length === 0
 }
