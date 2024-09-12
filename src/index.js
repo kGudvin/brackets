1569bf2f41d6f6158cf4586c446082181d696caa
Kirill Gudeta <kirillgudeta@gmail.com>
2024-09-13 01:08:43 +0300

    chat helped me to solve last 2

--- src/index.js
+++ src/index.js
@@ -1,37 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-	let op = 0 // Для отслеживания состояния символа '|'
 	const matching = {}
-
-	// Создаем таблицу соответствий закрывающих и открывающих скобок
-	bracketsConfig.map(el => {
-		matching[el[1]] = el[0]
-	})
-
-	const strArr = str.split('')
 	const stack = []
-
-	// Используем map() для обработки каждого элемента строки
-	strArr.map(el => {
-		if (Object.values(matching).includes(el)) {
-			if (el === '|' && op === 0) {
-				op += 1
-			} else if (el === '|' && op === 1) {
-				op -= 1
-				const x = stack.pop()
-				return
+	bracketsConfig.forEach(([open, close]) => {
+		matching[close] = open
+	})
+	for (const char of str) {
+		if (Object.values(matching).includes(char)) {
+			if (char === stack[stack.length - 1] && char === matching[char]) {
+				stack.pop()
+			} else {
+				stack.push(char)
 			}
-			stack.push(el)
-		} else if (Object.keys(matching).includes(el)) {
-			const x = stack.pop()
-			if (x === '|') {
-				op -= 1
-			}
-			if (x !== matching[el]) {
-				stack.push(x)
+		} else if (Object.keys(matching).includes(char)) {
+			if (stack.length === 0 || stack.pop() !== matching[char]) {
+				return false
 			}
 		}
-		return el
-	})
-
+	}
 	return stack.length === 0
 }
