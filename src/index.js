013bed8e799a94fc92c2db01c851fc6e10ae48bb
Kirill Gudeta <kirillgudeta@gmail.com>
2024-09-13 00:16:59 +0300

    done almost

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	const matching = {}
+	bracketsConfig.map(el => {
+		matching[`${el[1]}`] = `${el[0]}`
+	})
+	console.log(matching)
+	const strArr = str.split('')
+	const stack = []
+	strArr.map(el => {
+		if (Object.values(matching).includes(el)) {
+			stack.push(el)
+		}
+		if (Object.keys(matching).includes(el)) {
+			const x = stack.pop()
+			if (x !== matching[el]) {
+				stack.push(x)
+				return false
+			}
+		}
+		return el
+	})
+	return stack.length === 0
 }
